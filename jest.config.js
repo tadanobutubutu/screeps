d4d5c7b19800a805ac3f163c3037e36df3c948be
tadanobutubutu <58510538+tadanobutubutu@users.noreply.github.com>
2026-03-14 13:07:08 +0600

    feat: add Codecov coverage reporting with Jest

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  testEnvironment: 'node',
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '*.js',
+    '!jest.config.js',
+    '!node_modules/**',
+    '!coverage/**',
+  ],
+  coverageReporters: ['lcov', 'text', 'json-summary'],
+  coverageDirectory: 'coverage',
+  testMatch: [
+    '**/__tests__/**/*.js',
+    '**/*.test.js',
+    '**/*.spec.js',
+  ],
+  // Screeps globals
+  globals: {},
+  testPathIgnorePatterns: ['/node_modules/'],
+};

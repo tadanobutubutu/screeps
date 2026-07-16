97f34e438aad13e0ca67124521601a6d52e6fd1d
AI Issue Solver <ai-issue-solver@screeps.local>
2026-07-16 19:06:46 +0000

    fix: resolve issue #1558 -- concurrent multi-agent Sakana consensus [llm7]

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
  * Previous issues:
  *   • stray typographic quote
  *   • incomplete `getLodashVersion` function
- *   • confusing parenthesised `'use strict'` statement
+ *   • confusing parenthesised 'use strict' statement
  *   • dangling `r` character at the end
  *
  * The module now exports the helper functions for test consumption and
@@ -21,10 +21,10 @@
  * - `getTravisNodeVersion`
  * - `getRenovateUpdates`
  *
- * This module also re‑exports the version‑query functions defined in main.js
+ * This module also re-exports the version-query functions defined in main.js
  * so test files (or other modules) can import them straight from
  * `deploy.js`. The original file had a stray typographic quote at the
- * very first character (U+2019) which caused a parsing error. It’s been
+ * very first character (U+2019) which caused a parsing error. It's been
  * removed and the file is now a simple, clean wrapper.
  *
  * All code style issues have been resolved:
@@ -63,22 +63,22 @@ try {
 } catch (_) {}
 
 try {
-    const { version: v } = require('circleci/package.json');
+    const { version: v } = require('circleci/node/package.json');
     circleCIVersion = v;
 } catch (_) {}
 
 try {
-    const { version: v } = require('devcontainer/python/package.json');
+    const { version: v } = require('@devcontainer/template/package.json');
     devContainerPythonVersion = v;
 } catch (_) {}
 
 try {
-    const { version: v } = require('devcontainer/node/package.json');
+    const { version: v } = require('@devcontainer/template/package.json');
     devContainerNodeVersion = v;
 } catch (_) {}
 
 try {
-    const { version: v } = require('@travis-ci/node/package.json');
+    const { version: v } = require('travis-merge-deploy/package.json');
     travisNodeVersion = v;
 } catch (_) {}
 
@@ -89,7 +89,7 @@ try {
 } catch (_) {}
 
 /**
- * Gets the version of lodash from package.json.
+ * Gets the version of lodash from package. json.
  * @returns {string} The version of lodash
  */
 function getLodashVersion() {
@@ -97,7 +97,7 @@ function getLodashVersion() {
 }
 
 /**
- * Gets the version of PostHog from package.json.
+ * Gets the version of PostHog from package. json.
  * @returns {string} The version of PostHog
  */
 function getPostHogVersion() {
@@ -105,7 +105,7 @@ function getPostHogVersion() {
 }
 
 /**
- * Gets the version of Supabase from package.json.
+ * Gets the version of Supabase from package. json.
  * @returns {string} The version of Supabase
  */
 function getSupabaseVersion() {
@@ -113,7 +113,7 @@ function getSupabaseVersion() {
 }
 
 /**
- * Gets the version of CircleCI Node from package.json.
+ * Gets the version of CircleCI Node from package. json.
  * @returns {string} The version of CircleCI Node
  */
 function getCircleCINodeVersion() {
@@ -121,7 +121,7 @@ function getCircleCINodeVersion() {
 }
 
 /**
- * Gets the version of Dev Container Python from package.json.
+ * Gets the version of Dev Container Python from package. json.
  * @returns {string} The version of Dev Container Python
  */
 function getDevContainerPythonVersion() {
@@ -129,7 +129,7 @@ function getDevContainerPythonVersion() {
 }
 
 /**
- * Gets the version of Dev Container Node from package.json.
+ * Gets the version of Dev Container Node from package. json.
  * @returns {string} The version of Dev Container Node
  */
 function getDevContainerNodeVersion() {
@@ -137,7 +137,7 @@ function getDevContainerNodeVersion() {
 }
 
 /**
- * Gets the version of Travis Node from package.json.
+ * Gets the version of Travis Node from package. json.
  * @returns {string} The version of Travis Node
  */
 function getTravisNodeVersion() {
@@ -145,7 +145,7 @@ function getTravisNodeVersion() {
 }
 
 /**
- * Gets the version of Renovate updates from package.json.
+ * Gets the version of Renovate updates from package. json.
  * @returns {string} The version of Renovate
  */
 function getRenovateUpdates() {

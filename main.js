61d14c80a19d615ab31449132cd3e103767d79ec
AI Issue Solver <ai-issue-solver@screeps.local>
2026-08-22 10:34:01 +0000

    fix: resolve issue #14633 -- concurrent multi-agent Sakana consensus [kilo-gateway]

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 (function() {
-    var tableRotated = "<table id=\"table-rotated\"><thead><tr><th>Column 1</th><th>Column 2</th></tr></thead><tbody><tr><td>Data 1</td><td>Data 2</td></tr></tbody></table>";
+    var tableRotated = "<table id=\"table-rotated\"><thead><tr scope=\"row\"><th scope=\"col\">Column 1</th><th scope=\"col\">Column 2</th></tr></thead><tbody><tr><td>Data 1</td><td>Data 2</td></tr></tbody></table>";
     
     function renderMain() {
         var container = document.createElement('div');

be05de2388e101ae1ec4847196cd592259cf2b91
AI Issue Solver <ai-issue-solver@screeps.local>
2026-08-23 00:03:24 +0000

    fix: resolve issue #15610 -- concurrent multi-agent Sakana consensus [zhipu-glm]

--- main.js
+++ main.js
@@ -8,13 +8,15 @@ const AppLayout: React.FC = ({ children }) => {
   };
 
   return (
-    <div>
-      {/* Assuming some other components here */}
-      {children}
-      {/* Example of using the icons */}
-      <img src={icons.icon} alt="Screeps Dashboard Icon" />
-      <img src={icons.apple} alt="Screeps Apple Icon" />
-    </div>
+    <main>
+      <div>
+        {/* Assuming some other components here */}
+        {children}
+        {/* Example of using the icons */}
+        <img src={icons.icon} alt="Screeps Dashboard Icon" />
+        <img src={icons.apple} alt="Screeps Apple Icon" />
+      </div>
+    </main>
   );
 };
 

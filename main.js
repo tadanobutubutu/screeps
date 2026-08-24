362af48ba1528f2120562b723bb7233aa145fc7f
AI Issue Solver <ai-issue-solver@screeps.local>
2026-08-24 06:31:44 +0000

    fix: resolve issue #17891 -- concurrent multi-agent Sakana consensus [kilo-gateway]

--- main.js
+++ main.js
@@ -21,10 +21,20 @@ export function updateDependencyGraphHTML(html) {
   // Update the HTML file as follows:
   // Replace the <a id="unrotate" href="#">rotate back</a> with a <button id="unrotate" onclick="rotateBack()">rotate back</button>
   // Make sure to update the JavaScript to handle the button click if necessary
-  return html.replace(
+  // Also add scope="col" to <th> elements for accessibility (REACT_027)
+  let updatedHtml = html.replace(
     /<a id="unrotate" href="#">rotate back<\/a>/g,
     '<button id="unrotate" onclick="rotateBack()">rotate back</button>'
   );
+
+  // Add scope="col" to <th> elements that don't have a scope attribute
+  // This fixes the REACT_027 accessibility warning
+  updatedHtml = updatedHtml.replace(
+    /<th(?![^>]*\bscope=)([^>]*)>/g,
+    '<th scope="col"$1>'
+  );
+
+  return updatedHtml;
 }
 
 // ... any additional code that was present ...

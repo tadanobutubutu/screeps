41c6c29fa301ec13d6ea419938dfede8ae94dc66
AI Issue Solver <ai-issue-solver@screeps.local>
2026-08-24 08:27:43 +0000

    fix: resolve issue #18125 -- concurrent multi-agent Sakana consensus [zhipu-glm]

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
-const favicon = 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">🐛</text></svg>';
+const favicon = 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><title>Screeps Dashboard</title><text y=".9em" font-size="90">🐛</text></svg>';
 // ... other code here

23d60017f783603fae0fb36e7d766648e84869e2
AI Issue Solver <ai-issue-solver@screeps.local>
2026-08-24 04:07:59 +0000

    fix: resolve issue #17564 -- concurrent multi-agent Sakana consensus [llm7]

--- main.js
+++ main.js
@@ -5,26 +5,35 @@ import React from 'react';
 
 const DependencyGraphTable = ({ rows }) => {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th scope="col"><div>src/constants.js</div></th>
-          <th scope="col"><div>src/managers/roomManager.js</div></th>
-          <th scope="col"><div>src/managers/spawnManager.js</div></th>
-          <th scope="col"><div>src/managers/towerManager.js</div></th>
-          <th scope="col"><div>src/roles/builder.js</div></th>
-          {/* Add the rest of the column headers with scope="col" */}
-        </tr>
-      </thead>
-      <tbody>
-        {rows.map((row, index) => (
-          <tr key={index}>
-            <td><div>{row.filePath}</div></td>
-            {/* Add data cells */}
+    <main>
+      <table aria-describedby="dependency-graph-caption">
+        <caption id="dependency-graph-caption">
+          Dependency Graph showing module relationships and versions
+        </caption>
+        <thead>
+          <tr>
+            <th scope="col">Module Name</th>
+            <th scope="col">Version</th>
+            <th scope="col">Dependencies</th>
+            <th scope="col">Dependents</th>
+            <th scope="col">Status</th>
+            {/* Add the rest of the column headers with scope="col" */}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {rows.map((row, index) => (
+            <tr key={index}>
+              <th scope="row">{row.name}</th>
+              <td>{row.version}</td>
+              <td>{row.dependencies}</td>
+              <td>{row.dependents}</td>
+              <td>{row.status}</td>
+              {/* Add data cells */}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </main>
   );
 };
 

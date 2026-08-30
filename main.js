107ba51191b6a0a90d50f70df0f2f7d365807b14
AI Issue Solver <ai-issue-solver@screeps.local>
2026-08-30 04:31:57 +0000

    fix: resolve issue #71669 -- concurrent multi-agent Sakana consensus [llm7]

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ function deepClone(obj) {
   if (obj instanceof Object) {
     const cloned = {};
     for (const key in obj) {
-      if (obj.hasOwnProperty(key)) {
+      if (Object.prototype.hasOwnProperty.call(obj, key)) {
         cloned[key] = deepClone(obj[key]);
       }
     }

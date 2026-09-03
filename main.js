810a719d4fdc4ac11d02ca4643b560e4ff8aae0f
AI Issue Solver <ai-issue-solver@screeps.local>
2026-09-03 05:05:12 +0000

    fix: resolve issue #182292 -- concurrent multi-agent Sakana consensus [llm7]

--- main.js
+++ main.js
@@ -23,14 +23,10 @@ const addLanguageAttribute = () => {
 }
 
 const renderGraphIndex = (graphData) => {
+  addLanguageAttribute();
   addressAccessibilityIssues();
   renderDependencyGraphs(graphData);
 }
-
-// TODO: Update the existing function using the new functions for rendering graph/index
-// DO NOT REMOVE OR RENAME THE EXISTING FUNCTIONS BELOW
-
-// TODO: Add a language attribute to the HTML element
 
 // Update the call to the new function in the existing context
 // For instance, if there was a call to `renderDependencyGraphs` somewhere in the codebase, replace it with `renderGraphIndex`
